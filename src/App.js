546acfe06867148f1998e0eb7d953a309aad9b4a
akinadetunmise <akinade.tumise@outsourceglobal.com>
2021-11-01 22:09:47 +0100

    Changed Socket env

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import io from "socket.io-client";
 function App() {
   const [socket, setSocket] = useState(null);
   useEffect(() => {
-    const newSocket = io("http://localhost:3000");
+    const newSocket = io("http://34.240.246.145");
     setSocket(newSocket);
     console.log(newSocket);
     newSocket.io.on("error", (error) => {
