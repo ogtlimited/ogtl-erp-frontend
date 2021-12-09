c357846a5b58c03f6f8cb3a5771e6143ddc8b2d6
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2021-12-09 06:16:22 +0100

    updates signature

--- src/pages/In-Apps/signature.js
+++ src/pages/In-Apps/signature.js
@@ -1,12 +1,59 @@
 import React from 'react'
 import logo from '../../assets/img/outsource.png'
+import TwitterLogo from '../../assets/img/twitter.svg'
+import LinkedInLogo from '../../assets/img/linkedin.svg'
+import FacebookLogo from '../../assets/img/facebook.svg'
 const getIcon =  (name) => (<i class={"lab "+ name}></i> );
 
 const Signature = (props) => {
     return (
         <>
         <div className="row signature">
-        <div className="col-md-12 title">
+        <table cellPadding={0} cellSpacing={0} className={"signature"}>
+            <tbody>
+            <tr>
+                <td rowSpan={5}>
+                    <img
+                        className={"main-image"}
+                        src={props.photo === "no-photo" ? logo : props.photo}
+                        alt={""}
+                    />
+                </td>
+                <td rowSpan={5}>
+                    <div
+                        className={"vertical-line"}
+                    />
+                </td>
+                <td>{props.fullName}</td>
+            </tr>
+            <tr>
+                <td colSpan={3}>{props.position}</td>
+            </tr>
+            <tr>
+                <td colSpan={3}>Telephone: {props.phone}</td>
+            </tr>
+            <tr>
+                <td colSpan={3}>Skype: {props.skype}</td>
+            </tr>
+            <tr>
+                <td colSpan={3}>
+                    <div className={"social-logos-frame"}>
+                        <a href={"https://twitter.com/"}>
+                            <img src={TwitterLogo} alt={""}/>
+                        </a>
+                        <a href={"https://www.linkedin.com/"}>
+                            <img src={LinkedInLogo} alt={""}/>
+                        </a>
+                        <a href={"https://www.facebook.com/"}>
+                            <img src={FacebookLogo} alt={""}/>
+                        </a>
+                        
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        {/* <div className="col-md-12 title">
             <h1 className="s-title">{props.fullName}</h1>
             <p className="">{props.position}</p>
         </div>
@@ -41,7 +88,7 @@ const Signature = (props) => {
                 </div>
 
             </div>
-        </div>
+        </div> */}
 
         </div>
        
