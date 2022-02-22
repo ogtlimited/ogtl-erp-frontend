db50ac0a3a42308a2efbfe011de10710433f4738
amirdbt <ahmed.dambatta@outsourceglobal.com>
2022-02-22 12:09:29 +0100

    added quotes of the day and link to job openings

--- src/pages/HR/Users/Employee.User.js
+++ src/pages/HR/Users/Employee.User.js
@@ -1,10 +1,10 @@
 import moment from "moment";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import welcome from '../../../assets/img/welcome.png'
+import welcome from "../../../assets/img/welcome.png";
 import { useAppContext } from "../../../Context/AppContext";
 import axiosInstance from "../../../services/api";
-import holidays from './holidays.json'
+import holidays from "./holidays.json";
 import ogids from "./allEmployeeOgid.json";
 
 import personal from "./personal.json";
@@ -15,31 +15,34 @@ const EmployeeUser = () => {
   const [leaveTaken, setLeaveTaken] = useState(0);
   const [leaveRemaining, setLeaveRemaining] = useState(0);
 
-  const getNextHoliday = () =>{
-    const year = new Date().getFullYear()
-    const current = new Date().getTime()
-    const mapHolidays = holidays.map(hol => new Date(hol.date + " " + year).getTime() )
-    const greater = mapHolidays.filter(time => time >= current);
-    const index = mapHolidays.findIndex(idx => idx === Math.min(...greater))
+  const [jobOpeningsLength, setJobOpeningsLength] = useState("");
+  const [quotes, setQuotes] = useState(null);
 
-    return holidays[index]
-  }
+  const getNextHoliday = () => {
+    const year = new Date().getFullYear();
+    const current = new Date().getTime();
+    const mapHolidays = holidays.map((hol) =>
+      new Date(hol.date + " " + year).getTime()
+    );
+    const greater = mapHolidays.filter((time) => time >= current);
+    const index = mapHolidays.findIndex((idx) => idx === Math.min(...greater));
 
-  const calcShift = (time) =>{
-    if(time){
+    return holidays[index];
+  };
 
-      let split = time.split(":")
-      if(parseInt(split[0]) < 12){
-        return parseInt(split[0]) + ':' + split[1] + ' AM'
-      }else{
-        return parseInt(split[0]) + ':' + split[1] + ' PM'
-      } 
-    }else{
-      return ""
+  const calcShift = (time) => {
+    if (time) {
+      let split = time.split(":");
+      if (parseInt(split[0]) < 12) {
+        return parseInt(split[0]) + ":" + split[1] + " AM";
+      } else {
+        return parseInt(split[0]) + ":" + split[1] + " PM";
+      }
+    } else {
+      return "";
     }
-  }
+  };
   useEffect(() => {
-
     axiosInstance.get("/leave-application").then((e) => {
       const leaves = e?.data?.data?.filter(
         (f) => f?.employee_id?._id === user._id
@@ -56,6 +59,28 @@ const EmployeeUser = () => {
     });
   }, [user._id]);
 
+  useEffect(() => {
+    axiosInstance
+      .get("/api/jobOpening")
+      .then((res) => {
+        let newData = res.data.data.filter(
+          (dt) =>
+            moment(dt.createdAt).format("MMMM") === moment().format("MMMM")
+        );
+        setJobOpeningsLength(newData.length);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      const result = await fetch("https://quotes.rest/qod?language=en");
+      const data = await result.json();
+      setQuotes(data?.contents.quotes[0]);
+    };
+    fetchQuotes();
+  }, []);
   // let arr = []
   // let unique = []
   // let allShifts = []
@@ -92,92 +117,98 @@ const EmployeeUser = () => {
   // })
   return (
     <>
-
       <div className="row">
-      <div className="col-lg-8 col-md-8">
-        <div className="row welcome-card p-5">
-        <div className="col-md-9 left-card">
-        <h4 className="welcome-text">Welcome back,<br />  {`${user?.first_name} ${user?.middle_name} ${user?.last_name}`}{" "}!</h4>
-        <p className="welcome-p">If you havent punched in today, you need to do it right away</p>
-        <Link className="go" to="/dashboard/hr/attendance">Go Now</Link>
-        </div>
-        <div className="col-md-3">
-          <img style={{width: '100%'}} className="mt-4" src={welcome} />
-        </div>
-            
-        </div>
-        <div className="row mt-4">
-        <div className="col-lg-12 col-md-12">
-          <section className="dash-section">
-            <h1 className="dash-sec-title">Today</h1>
-            <div className="dash-sec-content">
-            <div className="dash-sec-content">
-              <div className="dash-info-list">
-                <div className="dash-card">
-                  <div className="dash-card-container">
-                    <div className="dash-card-icon">
-                      <i className="fa fa-clock"></i>
+        <div className="col-lg-8 col-md-8">
+          <div className="row welcome-card p-5">
+            <div className="col-md-9 left-card">
+              <h4 className="welcome-text">
+                Welcome back,
+                <br />{" "}
+                {`${user?.first_name} ${user?.middle_name} ${user?.last_name}`}{" "}
+                !
+              </h4>
+              <p className="welcome-p">
+                If you havent punched in today, you need to do it right away
+              </p>
+              <Link className="go" to="/dashboard/hr/attendance">
+                Go Now
+              </Link>
+            </div>
+            <div className="col-md-3">
+              <img style={{ width: "100%" }} className="mt-4" src={welcome} />
+            </div>
+          </div>
+          <div className="row mt-4">
+            <div className="col-lg-12 col-md-12">
+              <section className="dash-section">
+                <h1 className="dash-sec-title">Today</h1>
+                <div className="dash-sec-content">
+                  <div className="dash-sec-content">
+                    <div className="dash-info-list">
+                      <div className="dash-card">
+                        <div className="dash-card-container">
+                          <div className="dash-card-icon">
+                            <i className="fa fa-clock"></i>
+                          </div>
+                          <div className="dash-card-content">
+                            <p>
+                              Your shift starts at{" "}
+                              {calcShift(user?.default_shift?.start_time)} and
+                              ends at {calcShift(user?.default_shift?.end_time)}{" "}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <div className="dash-card-content">
-                      <p>Your shift starts at {calcShift(user?.default_shift?.start_time)} and ends at {calcShift(user?.default_shift?.end_time)} </p>
-                    </div>
-
                   </div>
                 </div>
-              </div>
+              </section>
 
-
-            </div>
-            </div>
-          </section>
-
-           <section className="dash-section">
-            <h1 className="dash-sec-title">This Month</h1>
-            <div className="dash-sec-content">
-              <div className="dash-info-list">
-                <div className="dash-card">
-                  <div className="dash-card-container">
-                    <div className="dash-card-icon">
-                      <i className="fa fa-suitcase"></i>
+              <section className="dash-section">
+                <h1 className="dash-sec-title">This Month</h1>
+                <div className="dash-sec-content">
+                  <div className="dash-info-list">
+                    <div className="dash-card">
+                      <div className="dash-card-container">
+                        <div className="dash-card-icon">
+                          <i className="fa fa-suitcase"></i>
+                        </div>
+                        <div className="dash-card-content">
+                          <p>You have 0 late attendance</p>
+                        </div>
+                      </div>
                     </div>
-                    <div className="dash-card-content">
-                      <p>You have 0 late attendance</p>
+                  </div>
+                  <div className="dash-info-list">
+                    <div className="dash-card">
+                      <div className="dash-card-container">
+                        <div className="dash-card-icon">
+                          <i className="fa fa-user-plus"></i>
+                        </div>
+                        <div className="dash-card-content">
+                          <p>Your first day is going to be on Thursday</p>
+                        </div>
+                      </div>
                     </div>
-
+                  </div>
+                  <div className="dash-info-list">
+                    <a href="" className="dash-card">
+                      <div className="dash-card-container">
+                        <div className="dash-card-icon">
+                          <i className="fa fa-calendar"></i>
+                        </div>
+                        <div className="dash-card-content">
+                          <p>It's Spring Bank Holiday on Monday</p>
+                        </div>
+                      </div>
+                    </a>
                   </div>
                 </div>
-              </div>
-              <div className="dash-info-list">
-                <div className="dash-card">
-                  <div className="dash-card-container">
-                    <div className="dash-card-icon">
-                      <i className="fa fa-user-plus"></i>
-                    </div>
-                    <div className="dash-card-content">
-                      <p>Your first day is going to be on Thursday</p>
-                    </div>
-                    
-                  </div>
-                </div>
-              </div>
-              <div className="dash-info-list">
-                <a href="" className="dash-card">
-                  <div className="dash-card-container">
-                    <div className="dash-card-icon">
-                      <i className="fa fa-calendar"></i>
-                    </div>
-                    <div className="dash-card-content">
-                      <p>It's Spring Bank Holiday on Monday</p>
-                    </div>
-                  </div>
-                </a>
-              </div>
+              </section>
             </div>
-          </section> 
+          </div>
         </div>
-        </div>
-      </div>
-       
+
         <div className="col-lg-4 col-md-4">
           <div className="dash-sidebar">
             <section>
@@ -218,46 +249,53 @@ const EmployeeUser = () => {
                     </div>
                   </div>
                   <div className="request-btn">
-                    <Link to="/dashboard/hr/leaves" className="btn btn-primary">Apply Leave</Link>
+                    <Link to="/dashboard/hr/leaves" className="btn btn-primary">
+                      Apply Leave
+                    </Link>
                   </div>
                 </div>
               </div>
             </section>
             <section>
-              <h5 className="dash-title">Your time off allowance</h5>
+              <h5 className="dash-title">Job Openings</h5>
               <div className="card">
                 <div className="card-body">
                   <div className="time-list">
                     <div className="dash-stats-list">
-                      <h4>0 Hours</h4>
-                      <p>Approved</p>
-                    </div>
-                    <div className="dash-stats-list">
-                      <h4>0 Hours</h4>
-                      <p>Remaining</p>
+                      <h4>{jobOpeningsLength} </h4>
+                      <p>new job openings</p>
                     </div>
                   </div>
                   <div className="request-btn">
-                    <button className="btn btn-primary">Apply Time Off</button>
+                    <a
+                      href="https://erp.outsourceglobal.com/recruitment/joblist"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="btn btn-primary"
+                    >
+                      View New Openings
+                    </a>
                   </div>
                 </div>
               </div>
             </section>
             <section>
-              <h5 className="dash-title">Upcoming Holiday</h5>
+              <h5 className="dash-title">Quote of the day</h5>
+
               <div className="card">
                 <div className="card-body text-center">
-                  <h4 className="holiday-title mb-0">
-                    {getNextHoliday()?.date} - {getNextHoliday()?.holiday_name}
-                  </h4>
+                  <figure>
+                    <blockquote>
+                      <h4 className="holiday-title">{quotes?.quote}</h4>
+                    </blockquote>
+                    <figcaption>—{quotes?.author}</figcaption>
+                  </figure>
                 </div>
               </div>
             </section>
           </div>
         </div>
-        
       </div>
-    
     </>
   );
 };
