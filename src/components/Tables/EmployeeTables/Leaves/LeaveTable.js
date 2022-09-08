82fdd7d5fc8aba0c9aa41e232c57ba2ecbbe2408
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-09-08 08:59:41 -0700

    Remove the select All row bugs

--- src/components/Tables/EmployeeTables/Leaves/LeaveTable.js
+++ src/components/Tables/EmployeeTables/Leaves/LeaveTable.js
@@ -108,7 +108,7 @@ const LeavesTable = ({
               <BootstrapTable
                 {...props.baseProps}
                 bordered={false}
-                selectRow={selectRow}
+                // selectRow={selectRow}
                 filter={filterFactory()}
                 headerClasses="header-class"
                 classes={
