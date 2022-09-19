0e839570fb8f9d0a26b3ba781b0fd3d3f6bec042
DhanteyUD <otseclinton60@gmail.com>
2022-09-19 16:36:48 +0100

    Fix

--- src/pages/Dashboard/AcademyRatio2.js
+++ src/pages/Dashboard/AcademyRatio2.js
@@ -1,3 +1,4 @@
+/** @format */
 
 import React, { useState, useEffect } from 'react';
 import { chartColors } from '../../components/charts/chart-colors';
@@ -9,15 +10,25 @@ import helper from '../../services/helper';
 const AcademyRatio2 = () => {
   const { combineRequest, showAlert } = useAppContext();
   const [applicantData, setApplicantData] = useState([]);
-  
+
   const initialChartState = { labels: [], datasets: [] };
   const [data, setdata] = useState(initialChartState);
+  const [data1, setData1] = useState([]);
 
   const fetchAcademyApplicants = () => {
     axiosInstance
       .get('/api/academy')
       .then((res) => {
+        const interested_position = {};
+        const intPos = res?.data?.data.map((item) => item.interested_position);
+        const interested_position_count = intPos.forEach((x) => {
+          interested_position[x] = (interested_position[x] || 0) + 1;
+        });
+
+        console.log('this interested position', interested_position);
+
         setApplicantData(res?.data?.data);
+        console.log('work with this data', res?.data?.data);
       })
       .catch((error) => {
         console.log(error);
