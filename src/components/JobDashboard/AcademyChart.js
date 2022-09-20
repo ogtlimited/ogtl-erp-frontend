c72e71763019c87ee1dcb9f94788c7fe402d328d
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2022-09-19 21:03:52 -0700

    Automating the overview chart

--- src/components/JobDashboard/AcademyChart.js
+++ src/components/JobDashboard/AcademyChart.js
@@ -1,38 +1,64 @@
-import { merge } from "lodash";
+import { isEmpty, merge } from "lodash";
+import { useState, useEffect } from "react";
 import ReactApexChart from "react-apexcharts";
 import BaseOptionChart from "../../utils/BaseOptionChart";
+import axiosInstance from "../../services/api";
+import moment from "moment";
+import { object } from "prop-types";
 
-const CHART_DATA = [
-  {
-    name: "Applicants",
-    type: "area",
-    data: [0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 0],
-  },
-  {
-    name: "Accepted Offers",
-    type: "area",
-    data: [0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 0],
-  },
-];
 
-export default function AcademyChart({ data }) {
+
+
+// export default function AcademyChart({ data }) {
+export default function AcademyChart() {
+  const [data, setData] = useState([])
+  const [applicationDate, setApplicationDate] = useState([])
+  const [numbersOfApplicants, setNumbersOfApplicants] = useState([])
+  const fetchAcademyApplicants = () => {
+    axiosInstance
+      .get('/api/academy')
+      .then((res) => {
+        setData(res.data.data)
+        setApplicationDate(res.data.data.map(item => (item.application_date) ))
+      })
+    }
+
+   
+    useEffect(() => {
+      fetchAcademyApplicants();
+    }, []);
+
+    const getOnlyDate = applicationDate.map(item=>(moment(item).format("MM/DD/YY")))
+    const sortedDate = getOnlyDate.sort()
+    const eachDate = sortedDate.map(item=> (item))
+
+    let uniqueDateCount = {};
+    for (let i=0; i<sortedDate.length; i++){
+      let num = sortedDate[i]
+
+      uniqueDateCount[num] = uniqueDateCount[num] ? uniqueDateCount[num] + 1 : 1
+
+    }
+   
+    const CHART_DATA = [
+      {
+        name: "Applicants",
+        type: "area",
+        data: Object.values(uniqueDateCount),
+      },
+      {
+        name: "Accepted Offers",
+        type: "area",
+        data: Object.values(uniqueDateCount),
+      },
+    ];
+        
   const chartOptions = merge(BaseOptionChart(), {
     stroke: { width: [0, 2, 3] },
     plotOptions: { bar: { columnWidth: "14%" } },
     fill: { type: ["solid", "gradient", "solid"] },
-    labels: [
-      "01/01/2022",
-      "02/01/2022",
-      "03/01/2022",
-      "04/01/2022",
-      "05/01/2022",
-      "06/01/2022",
-      "07/01/2022",
-      "08/01/2022",
-      "09/13/2022",
-      "09/14/2022",
-      "11/01/2022",
-    ],
+    labels: Object.keys(uniqueDateCount),
+  
     xaxis: { type: "datetime" },
     tooltip: {
       shared: true,
