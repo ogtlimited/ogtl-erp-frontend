c9d9a6a9432901ffefc0987f7731816aabcd29fa
DhanteyUD <clinton.otse@outsourceglobal.com>
2022-12-15 12:10:32 +0100

    Fix Status Cards on HR

--- src/pages/HR/Admin/Leaves.Admin.js
+++ src/pages/HR/Admin/Leaves.Admin.js
@@ -514,6 +514,24 @@ const LeavesAdmin = () => {
                 </h4>
               </div>
             </div>
+            <div className="col-md-3">
+              <div className="stats-info">
+                <h6>Pending Leaves</h6>
+                <h4>{pendingLeaves} &nbsp;</h4>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="stats-info">
+                <h6>Approved Leaves</h6>
+                <h4>{approvedLeaves}</h4>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="stats-info">
+                <h6>Rejected Leaves</h6>
+                <h4> {rejectedLeaves}</h4>
+              </div>
+            </div>
           </div>
           <AdminLeavesTable
             columns={columns}
