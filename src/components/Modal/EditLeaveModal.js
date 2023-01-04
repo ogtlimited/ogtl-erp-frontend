b185b96ca581f19d8e1c566980bd45abed3db1b3
DhanteyUD <clinton.otse@outsourceglobal.com>
2023-01-04 13:47:21 +0100

    Edit Leave Modal

--- src/components/Modal/EditLeaveModal.js
+++ src/components/Modal/EditLeaveModal.js
@@ -5,29 +5,72 @@ import { useAppContext } from '../../Context/AppContext';
 import { EDIT_LEAVE } from '../FormJSON/CreateLeave';
 import axiosInstance from '../../services/api';
 import $ from 'jquery';
+import ms from 'ms';
+import moment from 'moment';
 
 export const EditLeaveModal = ({ editLeave, fetchYourLeaves }) => {
   const { showAlert } = useAppContext();
   const [leave, setLeave] = useState(EDIT_LEAVE);
   const [loading, setLoading] = useState(false);
   const [leaveTypeOption, setLeaveTypeOption] = useState([]);
+  const [leaveTypeTitle, setLeaveTypeTitle] = useState('');
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const [today, setToday] = useState(null);
+  const [minDate, setMinDate] = useState(null);
+  const [maxDate, setMaxDate] = useState(null);
+
+  useEffect(() => {
+    const time = new Date().toDateString();
+    const today_date = moment(time).format('yyyy-MM-DD');
+    setToday(today_date);
+    const minSec = ms('15d');
+    const leaveDays = user.leaveCount + 15;
+    const maxSec = ms(`${leaveDays}d`);
+    const min_date = new Date(+new Date(today) + minSec);
+    const max_date = new Date(+new Date(today) + maxSec);
+    setMinDate(moment(min_date).format('yyyy-MM-DD'));
+    setMaxDate(moment(max_date).format('yyyy-MM-DD'));
+  }, [today, user.leaveCount]);
+
+  const fetchLeavesType = async () => {
+    try {
+      const response = await axiosInstance.get(`/leave-type`);
+      const resData = response?.data?.data;
+      console.log("leave type", resData)
+
+      setLeaveTypeOption(resData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const fetchLeavesTypeById = async (leaveId) => {
+    const id = leaveId;
+    try {
+      const response = await axiosInstance.get(`/leave-type/${id}`);
+      const resData = response?.data?.data.leave_type;
+
+      setLeaveTypeTitle(resData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleFormChange = (e) => {
     e.preventDefault();
     setLeave({ ...leave, [e.target.name]: e.target.value });
+
+    if (e.target.name === 'leave_type_id') {
+      fetchLeavesTypeById(e.target.value);
+    }
   };
+
   const handleEditLeave = async (e) => {
     e.preventDefault();
 
     setLoading(true);
     const id = editLeave._id;
-    console.log("Submit this edited leave:", 
-    {
-      leave_type_id: leave?.leave_type_id,
-      from_date: new Date(leave.from_date).toISOString(),
-      to_date: new Date(leave.to_date).toISOString(),
-      reason_for_application: leave.reason_for_application,
-    })
     try {
       const res = await axiosInstance.put(
         `leave-application/${id}`,
@@ -53,18 +96,6 @@ export const EditLeaveModal = ({ editLeave, fetchYourLeaves }) => {
     }
     setLoading(false);
   };
-
-  const fetchLeavesType = async () => {
-    try {
-      const response = await axiosInstance.get(`/leave-type`);
-      const resData = response?.data?.data;
-      console.log("leave type", resData)
-
-      setLeaveTypeOption(resData);
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   useEffect(() => {
     fetchLeavesType();
@@ -101,33 +132,6 @@ export const EditLeaveModal = ({ editLeave, fetchYourLeaves }) => {
                 <div className="row">
                   <div className="col-md-6">
                     <div className="form-group">
-                      <label htmlFor="from_date">From Date</label>
-                      <input
-                        type="date"
-                        name="from_date"
-                        value={leave.from_date}
-                        onChange={handleFormChange}
-                        className="form-control"
-                      />
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <label htmlFor="to_date">To Date</label>
-                      <input
-                        type="date"
-                        name="to_date"
-                        value={leave.to_date}
-                        onChange={handleFormChange}
-                        className="form-control"
-                      />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
                       <label htmlFor="leave_type_id">Leave Type</label>
                       <select
                         onChange={handleFormChange}
@@ -149,6 +153,7 @@ export const EditLeaveModal = ({ editLeave, fetchYourLeaves }) => {
                       </select>
                     </div>
                   </div>
+                  
                   <div className="col-md-6">
                     <div className="form-group">
                       <label htmlFor="reason_for_application">Reason</label>
@@ -158,6 +163,59 @@ export const EditLeaveModal = ({ editLeave, fetchYourLeaves }) => {
                         value={leave.reason_for_application}
                         onChange={handleFormChange}
                       />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label htmlFor="from_date">From Date</label>
+                      {leaveTypeTitle === 'Emergency Leave' ? (
+                        <input
+                          type="date"
+                          name="from_date"
+                          value={leave.from_date}
+                          onChange={handleFormChange}
+                          className="form-control "
+                          min={today}
+                        />
+                      ) : (
+                        <input
+                          type="date"
+                          name="from_date"
+                          value={leave.from_date}
+                          onChange={handleFormChange}
+                          className="form-control "
+                          min={minDate}
+                          max={maxDate}
+                        />
+                      )}
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label htmlFor="to_date">To Date</label>
+                      {leaveTypeTitle === 'Emergency Leave' ? (
+                        <input
+                          type="date"
+                          name="to_date"
+                          value={leave.to_date}
+                          onChange={handleFormChange}
+                          className="form-control "
+                          min={today}
+                        />
+                      ) : (
+                        <input
+                          type="date"
+                          name="to_date"
+                          value={leave.to_date}
+                          onChange={handleFormChange}
+                          className="form-control "
+                          min={minDate}
+                          max={maxDate}
+                        />
+                      )}
                     </div>
                   </div>
                 </div>
