e237a715715b1606d5b5c6e398ad26bde38853ac
DhanteyUD <clinton.otse@outsourceglobal.com>
2023-01-05 13:30:11 +0100

    Leave Type Table

--- src/components/Tables/EmployeeTables/leaveTypeTable.js
+++ src/components/Tables/EmployeeTables/leaveTypeTable.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 
 import React, { useState, useEffect } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
