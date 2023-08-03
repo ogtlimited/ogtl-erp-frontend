718960b6b29058a45a2609a7b665a002b5489a8e
Clinton Otse <111445852+clinton-otse@users.noreply.github.com>
2023-08-03 13:25:33 +0100

    Update Login.js

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -24,17 +24,17 @@ const Login = () => {
     msalInstance
       .ssoSilent(loginRequest)
       .then((e) => {
-        const activeUser = e?.account?.username;
+        // const activeUser = e?.account?.username;
 
         const obj = {
           company_email: data.company_email.trim(),
         };
 
-        if (obj.company_email !== activeUser) {
-          return setErrorMsg(
-            'There is an active user on this device'
-          );
-        }
+        // if (obj.company_email !== activeUser) {
+        //   return setErrorMsg(
+        //     'There is an active user on this device'
+        //   );
+        // }
 
         setErrorMsg("");
 
@@ -79,17 +79,17 @@ const Login = () => {
           msalInstance
             .loginPopup(loginRequest)
             .then((e) => {
-              const activeUser = e?.account?.username;
+              // const activeUser = e?.account?.username;
 
               const obj = {
                 company_email: data.company_email.trim(),
               };
 
-              if (obj.company_email !== activeUser) {
-                return setErrorMsg(
-                  'Please login with your credentials'
-                );
-              }
+              // if (obj.company_email !== activeUser) {
+              //   return setErrorMsg(
+              //     'Please login with your credentials'
+              //   );
+              // }
 
               setErrorMsg("");
 
