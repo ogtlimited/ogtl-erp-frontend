b6c3b08c06cf006cc985626c6e4bb2165059d21c
clinton-otse <clinton.otse@outsourceglobal.com>
2023-10-05 11:55:07 +0100

    Update Reports To

--- src/components/Modal/ReportToModal.js
+++ src/components/Modal/ReportToModal.js
@@ -141,7 +141,7 @@ export const ReportToModal = ({
           <div className="modal-content">
             <div className="modal-header">
               <h4 className="modal-title" id="FormModalLabel">
-                Report To
+                Update Reports To
               </h4>
               <button
                 type="button"
