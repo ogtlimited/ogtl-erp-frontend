ef081a34796e27d59cdbd5e9761cdb7802d8aef5
clinton-otse <clinton.otse@outsourceglobal.com>
2023-11-01 19:10:27 +0100

    Generate Payroll

--- src/components/Modal/GeneratePayrollModal.js
+++ src/components/Modal/GeneratePayrollModal.js
@@ -2,7 +2,6 @@
 /** @format */
 
 import React, { useState, useEffect } from "react";
-import { create_department } from "../FormJSON/CreateLeaveApprovalLevel";
 import { useAppContext } from "../../Context/AppContext";
 import axiosInstance from "../../services/api";
 import $ from "jquery";
@@ -13,15 +12,12 @@ export const GeneratePayrollModal = ({
   setGenerating,
 }) => {
   const { showAlert } = useAppContext();
-  const [createPayslips, setCreatePayslips] = useState({
-    start_time: "",
-    end_time: "",
-  });
+  const [createPayslips, setCreatePayslips] = useState("");
   const [loading, setLoading] = useState(false);
   const user = JSON.parse(secureLocalStorage.getItem("user"));
 
   const cancelEvent = () => {
-    setCreatePayslips(create_department);
+    setCreatePayslips("");
   };
 
   const handleFormChange = (e) => {
@@ -36,9 +32,13 @@ export const GeneratePayrollModal = ({
     e.preventDefault();
 
     setLoading(true);
+
+    const month = createPayslips.monthAndYear.split("-")[1];
+    const year = createPayslips.monthAndYear.split("-")[0];
+
     try {
       const res = await axiosInstance.post(
-        `/api/v1/salary_slips.json?start_date=${createPayslips.start_time}&end_date=${createPayslips.end_time}`,
+        `/api/v1/salary_slips.json?month=${month}&year=${year}`,
         {
           headers: {
             "Content-Type": "application/json",
@@ -53,7 +53,7 @@ export const GeneratePayrollModal = ({
         `Salary slips are being generated`,
         "alert alert-success"
       );
-      setCreatePayslips(create_department);
+      setCreatePayslips("");
       $("#GeneratePayrollModal").modal("toggle");
       fetchEmployeeSalarySlip();
       setGenerating(false);
@@ -96,31 +96,17 @@ export const GeneratePayrollModal = ({
                 <div className="row">
                   <div className="col-md-12">
                     <div className="form-group">
-                      <label htmlFor="start_time">Select Month & Year</label>
+                      <label htmlFor="monthAndYear">Select Month & Year</label>
                       <input
-                        name="start_time"
+                        name="monthAndYear"
                         type="month"
                         className="form-control"
-                        value={createPayslips.start_time}
+                        value={createPayslips.month}
                         onChange={handleFormChange}
                         required
                       />
                     </div>
                   </div>
-
-                  {/* <div className="col-md-6">
-                    <div className="form-group">
-                      <label htmlFor="end_time">Year</label>
-                      <input
-                        name="end_time"
-                        type="year"
-                        className="form-control"
-                        value={createPayslips.end_time}
-                        onChange={handleFormChange}
-                        required
-                      />
-                    </div>
-                  </div> */}
                 </div>
 
                 <div className="modal-footer">
