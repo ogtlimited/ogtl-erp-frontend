3848eb7b36a6f19e485fccce752af3248199a03b
clinton-otse <clinton.otse@outsourceglobal.com>
2024-02-29 12:17:11 +0100

    eCertificate
    Employee Engagement

--- src/pages/HR/Users/EmployeeAppreciation.User.js
+++ src/pages/HR/Users/EmployeeAppreciation.User.js
@@ -1,8 +1,17 @@
 //* IN USE
 
 import React from "react";
+import { useAppContext } from "../../../Context/AppContext";
+import eCertificate from "../../../assets/img/Certificate of Appreciation.jpg";
 
 const EmployeeAppreciation = () => {
+  const { user } = useAppContext();
+
+  const employeeName = user?.employee_info?.personal_details
+  const {first_name, last_name} = employeeName;
+
+  console.log(employeeName)
+
   return (
     <>
       <div className="page-header">
@@ -14,9 +23,20 @@ const EmployeeAppreciation = () => {
               <li className="breadcrumb-item active">Employee Appreciation</li>
             </ul>
           </div>
+          <div className="col-auto float-right ml-auto">
+            <button className="btn add-btn m-r-5">Download</button>
+          </div>
+        </div>
+
+        <div className="eCertificate_container">
+          <div className="eCertificate_img_wrapper">
+            <p>{first_name} {last_name}</p>
+            <img src={eCertificate} alt="Certificate of Appreciation" />
+          </div>
         </div>
       </div>
     </>
   );
 };
+
 export default EmployeeAppreciation;
