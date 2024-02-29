0b0388b9546740cc0d7091b1aeef0c7c1c72590d
clinton-otse <clinton.otse@outsourceglobal.com>
2024-02-29 16:02:44 +0100

    CEO Message

--- src/components/ModalContents/CEOMessageContent.js
+++ src/components/ModalContents/CEOMessageContent.js
@@ -1,5 +1,82 @@
 const CEOMessageContent = () => {
-  return <div className="row d-flex justify-content-center">CEO Message</div>;
+  return (
+    <div
+      className="row d-flex justify-content-center"
+      style={{ padding: "0 2rem" }}
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. In ante metus dictum
+      at. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum.
+      Netus et malesuada fames ac. Velit laoreet id donec ultrices. Nisl
+      condimentum id venenatis a condimentum. Praesent semper feugiat nibh sed
+      pulvinar. Molestie at elementum eu facilisis sed odio. Sit amet nulla
+      facilisi morbi. Ullamcorper velit sed ullamcorper morbi tincidunt ornare
+      massa eget. Leo integer malesuada nunc vel. Velit laoreet id donec
+      ultrices tincidunt arcu. Lectus magna fringilla urna porttitor rhoncus
+      dolor purus non. Adipiscing bibendum est ultricies integer quis auctor
+      elit sed vulputate.
+      <br />
+      <br />
+      Mollis nunc sed id semper risus in hendrerit. Eget nunc lobortis mattis
+      aliquam faucibus purus in massa tempor. Maecenas volutpat blandit aliquam
+      etiam erat. Sit amet cursus sit amet dictum sit amet. Vitae tempus quam
+      pellentesque nec nam aliquam sem et. Sagittis orci a scelerisque purus
+      semper eget duis. Eu lobortis elementum nibh tellus molestie nunc. Purus
+      in massa tempor nec feugiat. Ut faucibus pulvinar elementum integer enim
+      neque volutpat ac. At lectus urna duis convallis convallis. Fermentum et
+      sollicitudin ac orci phasellus egestas tellus rutrum tellus. Pharetra
+      convallis posuere morbi leo urna. Duis convallis convallis tellus id
+      interdum velit laoreet id donec.
+      <br />
+      <br />
+      Accumsan lacus vel facilisis volutpat est velit egestas dui id. Lorem
+      mollis aliquam ut porttitor leo. Ultricies integer quis auctor elit sed
+      vulputate mi sit amet. Ut consequat semper viverra nam libero justo
+      laoreet. Hendrerit gravida rutrum quisque non tellus orci. Quam lacus
+      suspendisse faucibus interdum posuere lorem ipsum dolor. Sit amet volutpat
+      consequat mauris nunc congue nisi vitae. Rhoncus urna neque viverra justo
+      nec ultrices. Orci dapibus ultrices in iaculis nunc sed. Porttitor eget
+      dolor morbi non. Eu ultrices vitae auctor eu augue ut lectus arcu. Sed
+      nisi lacus sed viverra tellus in hac habitasse platea. Pellentesque
+      habitant morbi tristique senectus et netus.
+      <br />
+      <br />
+      Mattis aliquam faucibus purus in. Hendrerit dolor magna eget est lorem.
+      Placerat vestibulum lectus mauris ultrices eros in. In arcu cursus euismod
+      quis viverra nibh cras pulvinar. Praesent tristique magna sit amet purus.
+      Dui accumsan sit amet nulla facilisi morbi tempus iaculis. Ac odio tempor
+      orci dapibus. Et ultrices neque ornare aenean euismod. Risus viverra
+      adipiscing at in tellus integer feugiat. Cras semper auctor neque vitae
+      tempus quam pellentesque nec.
+      <br />
+      <br />
+      Nisi est sit amet facilisis magna etiam tempor orci eu. Id volutpat lacus
+      laoreet non curabitur gravida arcu ac tortor. Purus ut faucibus pulvinar
+      elementum integer enim. Ornare quam viverra orci sagittis eu volutpat.
+      Adipiscing at in tellus integer feugiat scelerisque varius. Nisl suscipit
+      adipiscing bibendum est ultricies integer. Nec ultrices dui sapien eget mi
+      proin. Pellentesque habitant morbi tristique senectus. Velit aliquet
+      sagittis id consectetur purus. Pulvinar pellentesque habitant morbi
+      tristique senectus et. Vestibulum mattis ullamcorper velit sed. Ipsum
+      dolor sit amet consectetur adipiscing. Imperdiet sed euismod nisi porta
+      lorem mollis aliquam ut. Molestie a iaculis at erat pellentesque
+      adipiscing commodo. Suscipit adipiscing bibendum est ultricies. Arcu dui
+      vivamus arcu felis bibendum ut tristique et egestas. Faucibus vitae
+      aliquet nec ullamcorper sit amet. Dolor purus non enim praesent elementum
+      facilisis leo.
+      <br />
+      <br />
+      <div className="signature_wrapper">
+        <div className="signature_container">
+          <img
+            src="https://res.cloudinary.com/dhantey/image/upload/v1679519740/signature-placeholder_xykxnx.png"
+            alt="Employee Signature"
+          />
+          <p>AMAL HASSAN (CEO)</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CEOMessageContent;
