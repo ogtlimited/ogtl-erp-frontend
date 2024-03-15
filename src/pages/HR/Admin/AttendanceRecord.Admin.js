87900ad74863527bfd2e637c534240119bbfa838
clinton-otse <clinton.otse@outsourceglobal.com>
2024-03-15 10:29:53 +0100

    Attendance Fix

--- src/pages/HR/Admin/AttendanceRecord.Admin.js
+++ src/pages/HR/Admin/AttendanceRecord.Admin.js
@@ -45,61 +45,6 @@ const AttendanceRecord = () => {
   const [officeType, setOfficeType] = useState("");
   const [officeId, setOfficeId] = useState("");
 
-  // ! This code is for generating dynamic date columns for the Monthly Attendance table | without clock in and clock out times
-  // Monthly Attendance - Dynamic Date Columns:
-  // useEffect(() => {
-  //   const attendanceDates =
-  //     monthlyAttendance.map((entry) => entry.date) || [];
-
-  //   const dateColumns = attendanceDates.map((date) => ({
-  //     dataField: `attendance_${date}`,
-  //     text: moment(date).format("DD-MMM-YYYY"),
-  //     headerStyle: { width: "100%" },
-  //     formatter: (cell, row) => {
-  //       const attendanceRecord = row.attendance.find(
-  //         (entry) => entry?.date === date
-  //       );
-
-  //       let backgroundColor = "";
-  //       let color = "";
-  //       if (attendanceRecord) {
-  //         switch (attendanceRecord.status) {
-  //           case "Absent":
-  //             backgroundColor = "#BE3144";
-  //             color = "#ffffff";
-  //             break;
-  //           case "Was Sick":
-  //             backgroundColor = "#6f42c1";
-  //             color = "#ffffff";
-  //             break;
-  //           case "On Leave":
-  //             backgroundColor = "#20c997";
-  //             color = "#ffffff";
-  //             break;
-  //           default:
-  //             backgroundColor = "";
-  //             color = "";
-  //         }
-  //       }
-
-  //       return (
-  //         <div
-  //           style={{
-  //             textAlign: "center",
-  //              backgroundColor: backgroundColor,
-  //              padding: attendanceRecord.status === "Absent" ? "5px 20px" : "",
-  //              color: color,
-  //           }}
-  //         >
-  //           {attendanceRecord ? attendanceRecord.status : ""}
-  //         </div>
-  //       );
-  //     },
-  //   }));
-
-  //   setDateColumns(dateColumns);
-  // }, [monthlyAttendance]);
-
   useEffect(() => {
     const allDates = Array.from(
       new Set(
@@ -231,6 +176,7 @@ const AttendanceRecord = () => {
           : response?.data?.data?.info.map((e, index) => ({
               ...e,
               idx: index + 1,
+              office: e?.office ? e?.office?.toUpperCase() : null,
               date: moment(e?.date).format("ddd, Do MMM. YYYY"),
             }));
 
@@ -342,7 +288,7 @@ const AttendanceRecord = () => {
         })),
       }));
 
-      console.log("formatted", formattedData);
+      // console.log("formatted", formattedData);
 
       setSizePerPage(sizePerPage);
       setTotalPages(recordPages);
@@ -451,7 +397,7 @@ const AttendanceRecord = () => {
       dataField: "full_name",
       text: "Employee",
       sort: true,
-      headerStyle: { width: "20%" },
+      headerStyle: { width: "100%" },
       formatter: (value, row) => (
         <h2 className="table-avatar">
           <span
@@ -472,25 +418,25 @@ const AttendanceRecord = () => {
       dataField: "ogid",
       text: "OGID",
       sort: true,
-      headerStyle: { width: "10%" },
+      headerStyle: { width: "100%" },
     },
     {
       dataField: "office",
       text: "Office",
       sort: true,
-      headerStyle: { width: "10%" },
+      headerStyle: { width: "100%" },
     },
     {
       dataField: "date",
       text: "Date",
       sort: true,
-      headerStyle: { width: "15%" },
+      headerStyle: { width: "100%" },
     },
     {
       dataField: "clock_in",
       text: "Clock In",
       sort: true,
-      headerStyle: { width: "15%" },
+      headerStyle: { width: "100%" },
       formatter: (value) => {
         return value === "No Clock in" ? (
           <span className="text-danger">{value}</span>
@@ -503,7 +449,7 @@ const AttendanceRecord = () => {
       dataField: "clock_out",
       text: "Clock Out",
       sort: true,
-      headerStyle: { width: "15%" },
+      headerStyle: { width: "100%" },
       formatter: (value) => {
         return value === "No Clock out" ? (
           <span className="text-danger">{value}</span>
@@ -516,7 +462,7 @@ const AttendanceRecord = () => {
       dataField: "work_hours",
       text: "Work Hours",
       sort: true,
-      headerStyle: { width: "15%" },
+      headerStyle: { width: "100%" },
     },
   ];
 
