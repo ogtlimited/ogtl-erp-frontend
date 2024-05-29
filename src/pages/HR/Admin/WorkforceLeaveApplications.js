544a4ff8f319ecde3b26f642beb901afe0270f22
clinton-otse <clinton.otse@outsourceglobal.com>
2024-05-29 12:34:22 +0100

    Workforce Leave Fix

--- src/pages/HR/Admin/WorkforceLeaveApplications.js
+++ src/pages/HR/Admin/WorkforceLeaveApplications.js
@@ -1,46 +1,54 @@
+//* IN USE
+
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 import React, { useState, useEffect, useCallback } from "react";
+import { useAppContext } from "../../../Context/AppContext";
 import AdminLeavesTable from "../../../components/Tables/EmployeeTables/Leaves/AdminLeaveTable";
 import AdminLeavesHistoryTable from "../../../components/Tables/EmployeeTables/Leaves/AdminLeaveHistoryTable";
-import axiosInstance from "../../../services/api";
-import { useAppContext } from "../../../Context/AppContext";
-import ViewModal from "../../../components/Modal/ViewModal";
 import LeaveApplicationContent from "../../../components/ModalContents/LeaveApplicationContent";
 import RejectWorkforceLeaveModal from "../../../components/Modal/RejectWorkforceLeaveModal";
+import ViewModal from "../../../components/Modal/ViewModal";
+import axiosInstance from "../../../services/api";
 import moment from "moment";
 
 const WorkforceLeaveApplications = () => {
-  const [allLeaves, setallLeaves] = useState([]);
-  const [leaveHistory, setLeaveHistory] = useState([]);
   const { showAlert, user, ErrorHandler, getAvatarColor } = useAppContext();
+
+  const [allLeaves, setallLeaves] = useState([]);
+  const [approvedLeaves, setApprovedLeaves] = useState([]);
+  const [leaveHistory, setLeaveHistory] = useState([]);
   const [modalType, setmodalType] = useState("");
   const [viewRow, setViewRow] = useState(null);
+
   const [loading, setLoading] = useState(false);
+  const [loadingApproved, setLoadingApproved] = useState(false);
   const [loadingHistory, setLoadingHistory] = useState(false);
+
   const [rejectModal, setRejectModal] = useState(false);
   const [dataManagerReject, setDataManagerReject] = useState([]);
-  const [historyStatus, setHistoryStatus] = useState("approved");
+  const [historyStatus, setHistoryStatus] = useState("rejected");
 
   const [page, setPage] = useState(1);
   const [sizePerPage, setSizePerPage] = useState(10);
   const [totalPages, setTotalPages] = useState("");
 
+  const [approvedPage, setApprovedPage] = useState(1);
+  const [approvedSizePerPage, setApprovedSizePerPage] = useState(10);
+  const [approvedTotalPages, setApprovedTotalPages] = useState("");
+
   const [historyPage, setHistoryPage] = useState(1);
   const [historySizePerPage, setHistorySizePerPage] = useState(10);
   const [historyTotalPages, setHistoryTotalPages] = useState("");
 
-  const firstDay = moment().startOf("month").format("YYYY-MM-DD");
-  const lastDay = moment().endOf("month").format("YYYY-MM-DD");
-  const [fromDate, setFromDate] = useState(firstDay);
-  const [toDate, setToDate] = useState(lastDay);
-  const [today, setToday] = useState(null);
-
-  useEffect(() => {
-    const today_date = moment().utc().format("yyyy-MM-DD");
-    setToday(today_date);
-  }, []);
+  const [fromDate, setFromDate] = useState(
+    moment().startOf("month").format("YYYY-MM-DD")
+  );
+  const [toDate, setToDate] = useState(
+    moment().endOf("month").format("YYYY-MM-DD")
+  );
+  const [today] = useState(moment().utc().format("yyyy-MM-DD"));
 
   const isWorkforceManager = user?.employee_info?.roles.includes("data_manager")
     ? true
@@ -118,6 +126,71 @@ const WorkforceLeaveApplications = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page, sizePerPage]);
 
+  useEffect(() => {
+    if (isWorkforceManager) {
+      fetchWorkforceLeaves();
+    }
+  }, [fetchWorkforceLeaves, isWorkforceManager]);
+
+  // All Leaves at Workforce stage - Approved
+  const fetchApprovedWorkforceLeaves = useCallback(async () => {
+    setLoadingApproved(true);
+    try {
+      const response = await axiosInstance.get(`/api/v1/approved_leaves.json`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "ngrok-skip-browser-warning": "69420",
+        },
+        params: {
+          pages: page,
+          limit: sizePerPage,
+        },
+      });
+      const resData = response?.data?.data?.leaves;
+
+      console.log("Approved leaves:", resData);
+
+      // const totalPages = response?.data?.data?.total_pages;
+
+      // setSizePerPage(sizePerPage);
+      // setTotalPages(totalPages);
+
+      // const formatted = resData.map((leave) => ({
+      //   ...leave,
+      //   ...leave?.leave,
+      //   office: leave.office.toUpperCase(),
+      //   full_name:
+      //     leave?.first_name.toUpperCase() +
+      //     " " +
+      //     leave?.last_name.toUpperCase(),
+      //   from_date: moment(leave?.leave?.start_date).format(
+      //     "YYYY, MM (MMM), Do"
+      //   ),
+      //   to_date: moment(leave?.leave?.end_date).format("YYYY, MM (MMM), Do"),
+      //   total_leave_days: calcBusinessDays(
+      //     leave?.leave?.start_date,
+      //     leave?.leave?.end_date
+      //   ),
+      //   date_applied: moment(leave?.leave?.created_at).format("Do MMM, YYYY"),
+      // }));
+
+      // setallLeaves(formatted);
+      // setLoadingApproved(false);
+    } catch (error) {
+      const component = "Approved Leave Error | ";
+      ErrorHandler(error, component);
+      setLoadingApproved(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, sizePerPage]);
+
+  useEffect(() => {
+    if (isWorkforceManager) {
+      fetchApprovedWorkforceLeaves();
+    }
+  }, [fetchApprovedWorkforceLeaves, isWorkforceManager]);
+
   // All Leaves at Workforce stage - History
   const fetchWorkforceLeaveHistory = useCallback(async () => {
     try {
@@ -134,8 +207,6 @@ const WorkforceLeaveApplications = () => {
             pages: historyPage,
             limit: historySizePerPage,
             status: historyStatus,
-            from_date: fromDate,
-            to_date: toDate,
           },
         }
       );
@@ -186,10 +257,9 @@ const WorkforceLeaveApplications = () => {
 
   useEffect(() => {
     if (isWorkforceManager) {
-      fetchWorkforceLeaves();
       fetchWorkforceLeaveHistory();
     }
-  }, [fetchWorkforceLeaves, fetchWorkforceLeaveHistory, isWorkforceManager]);
+  }, [fetchWorkforceLeaveHistory, isWorkforceManager]);
 
   // Approve Leave
   const handleApproveLeave = async (row) => {
@@ -419,6 +489,216 @@ const WorkforceLeaveApplications = () => {
                 <i className="fa fa-ban m-r-5"></i> Reject
               </a>
             ) : null}
+          </div>
+        </div>
+      ),
+    },
+  ];
+
+  const approvedColumns = [
+    {
+      dataField: "full_name",
+      text: "Employee",
+      sort: true,
+      headerStyle: { width: "100%" },
+      formatter: (value, row) => (
+        <h2 className="table-avatar">
+          <span
+            className="avatar-span"
+            style={{ backgroundColor: getAvatarColor(value?.charAt(0)) }}
+          >
+            {value?.charAt(0)}
+          </span>
+          <div>
+            {value} <span>{row?.ogid}</span>
+          </div>
+        </h2>
+      ),
+    },
+    {
+      dataField: "date_applied",
+      text: "Date Applied",
+      sort: true,
+      headerStyle: { width: "100%" },
+    },
+    {
+      dataField: "office",
+      text: "Office",
+      sort: true,
+      headerStyle: { width: "100%" },
+      formatter: (val, row) => <span>{val}</span>,
+    },
+    {
+      dataField: "status",
+      text: "Status",
+      sort: true,
+      headerStyle: { width: "100%" },
+      formatter: (value, row) => (
+        <>
+          {value === "approved" ? (
+            <span className="btn btn-gray btn-sm btn-rounded">
+              <i className="fa fa-dot-circle-o text-success"></i> {value}
+            </span>
+          ) : value === "cancelled" ? (
+            <span className="btn btn-gray btn-sm btn-rounded">
+              <i className="fa fa-dot-circle-o text-primary"></i> {value}
+            </span>
+          ) : value === "rejected" ? (
+            <span className="btn btn-gray btn-sm btn-rounded">
+              <i className="fa fa-dot-circle-o text-danger"></i> {value}
+            </span>
+          ) : value === "pending" ? (
+            <span className="btn btn-gray btn-sm btn-rounded ">
+              <i className="fa fa-dot-circle-o text-warning"></i> {value}
+            </span>
+          ) : null}
+        </>
+      ),
+    },
+    {
+      dataField: "leave_type",
+      text: "Leave Type",
+      sort: true,
+      headerStyle: { width: "100%" },
+    },
+    {
+      dataField: "leave_marker",
+      text: "Leave Progress",
+      sort: true,
+      headerStyle: { width: "100%", textAlign: "center" },
+      formatter: (value, row) => (
+        <>
+          {row?.status === "approved" ? (
+            <>
+              {value === "Scheduled Leave" ? (
+                <span className="btn btn-gray btn-sm btn-rounded">
+                  <i className="fa fa-dot-circle-o text-warning"></i> {value}
+                </span>
+              ) : value === "On Leave" ? (
+                <span className="btn btn-gray btn-sm btn-rounded">
+                  <i className="fa fa-dot-circle-o text-success"></i> {value}
+                </span>
+              ) : value === "Leave Ended" ? (
+                <span className="btn btn-gray btn-sm btn-rounded">
+                  <i className="fa fa-dot-circle-o text-danger"></i> {value}
+                </span>
+              ) : null}
+            </>
+          ) : (
+            <>
+              <span className="btn btn-gray btn-sm btn-rounded">
+                <i className="fa fa-dot-circle-o text-secondary"></i> Not
+                Approved
+              </span>
+            </>
+          )}
+        </>
+      ),
+    },
+    {
+      dataField: "from_date",
+      text: "From",
+      sort: true,
+      headerStyle: { width: "100%" },
+    },
+    {
+      dataField: "to_date",
+      text: "To",
+      sort: true,
+      headerStyle: { width: "100%" },
+    },
+    {
+      dataField: "total_leave_days",
+      text: "Total Leave Days",
+      sort: true,
+      headerStyle: { width: "100%", textAlign: "center" },
+      formatter: (value, row) => (
+        <>
+          {row.total_leave_days > 1
+            ? row.total_leave_days + " days"
+            : row.total_leave_days + " day"}
+        </>
+      ),
+    },
+    {
+      dataField: "proofs",
+      text: "Proofs",
+      sort: true,
+      headerStyle: { width: "100%" },
+      formatter: (value, row) => (
+        <>
+          {row?.proofs ? (
+            <>
+              {row?.proofs.length > 1 ? (
+                <div className="dropdown">
+                  <button
+                    className="btn btn-sm btn-primary dropdown-toggle"
+                    type="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Downloads
+                  </button>
+                  <div className="dropdown-menu">
+                    {row?.proofs.map((proof, index) => (
+                      <a
+                        key={index}
+                        href={proof}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="dropdown-item"
+                        download
+                      >
+                        <i className="fa fa-download"></i> Download {index + 1}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              ) : (
+                <a
+                  href={value[0]}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-sm btn-primary"
+                  download
+                >
+                  <i className="fa fa-download"></i> Download
+                </a>
+              )}
+            </>
+          ) : null}
+        </>
+      ),
+    },
+    {
+      dataField: "status_action",
+      text: "Action",
+      csvExport: false,
+      headerStyle: { width: "100%" },
+      formatter: (value, row) => (
+        <div className="dropdown dropdown-action text-right">
+          <a
+            href="#"
+            className="action-icon dropdown-toggle"
+            data-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
+          </a>
+          <div className="dropdown-menu dropdown-menu-right">
+            <a
+              className="dropdown-item"
+              href="#"
+              data-toggle="modal"
+              data-target="#generalModal"
+              onClick={() => {
+                setmodalType("view-details");
+                setViewRow(row);
+              }}
+            >
+              <i className="fa fa-eye m-r-5"></i> View
+            </a>
           </div>
         </div>
       ),
@@ -677,7 +957,7 @@ const WorkforceLeaveApplications = () => {
                 <a
                   className="nav-link active"
                   data-toggle="tab"
-                  href="#tab_hr-leave-application"
+                  href="#tab_workforce-pending-leave"
                 >
                   Leave Applications
                 </a>
@@ -686,17 +966,16 @@ const WorkforceLeaveApplications = () => {
                 <a
                   className="nav-link"
                   data-toggle="tab"
-                  href="#tab_hr-leave-history"
-                  onClick={() => setHistoryStatus("approved")}
+                  href="#tab_tab_workforce-approved-leave"
                 >
-                  Acted On
+                  Approved
                 </a>
               </li>
               <li className="nav-item">
                 <a
                   className="nav-link"
                   data-toggle="tab"
-                  href="#tab_hr-leave-history"
+                  href="#tab_workforce-leave-history"
                   onClick={() => setHistoryStatus("rejected")}
                 >
                   Rejected Leave History
@@ -709,7 +988,7 @@ const WorkforceLeaveApplications = () => {
 
       <div className="row tab-content">
         <div
-          id="tab_hr-leave-application"
+          id="tab_workforce-pending-leave"
           className="col-12 tab-pane show active"
         >
           <AdminLeavesTable
@@ -726,8 +1005,8 @@ const WorkforceLeaveApplications = () => {
             setTotalPages={setTotalPages}
           />
         </div>
-        
-        <div id="tab_hr-leave-history" className="col-12 tab-pane">
+
+        <div id="tab_tab_workforce-approved-leave" className="col-12 tab-pane">
           <div className="row col-md-6">
             <div className="col-md-6">
               <div className="form-group">
@@ -757,6 +1036,22 @@ const WorkforceLeaveApplications = () => {
             </div>
           </div>
 
+          <AdminLeavesHistoryTable
+            columns={approvedColumns}
+            data={approvedLeaves}
+            setData={setApprovedLeaves}
+            loading={loadingApproved}
+            setLoading={setLoadingApproved}
+            page={approvedPage}
+            setPage={setApprovedPage}
+            sizePerPage={approvedSizePerPage}
+            setSizePerPage={setApprovedSizePerPage}
+            totalPages={approvedTotalPages}
+            setTotalPages={setApprovedTotalPages}
+          />
+        </div>
+
+        <div id="tab_workforce-leave-history" className="col-12 tab-pane">
           <AdminLeavesHistoryTable
             columns={historyColumns}
             data={leaveHistory}
