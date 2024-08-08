03b740f011b28ac6b13556ea3ac452c0e4da61e6
clinton-otse <clinton.otse@outsourceglobal.com>
2024-08-08 12:42:05 +0100

    approval module

--- src/pages/HR/Admin/ApprovalModule.Admin.js
+++ src/pages/HR/Admin/ApprovalModule.Admin.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axiosInstance from "../../../services/api";
 import { useAppContext } from "../../../Context/AppContext";
 import {
   selectModuleType,
   officeTypeOptions,
-  selectApproverOptions
+  selectApproverOptions,
+  selectLeadershipOptions
 } from "../../../components/FormJSON/Select";
 import { MdOutlineAdd } from "react-icons/md";
 import Select from "react-select";
@@ -18,6 +19,7 @@ const ApprovalModule = () => {
     loadingSelect,
     selectDepartments,
     selectCampaigns,
+    selectDesignations,
     goToTop
   } = useAppContext();
 
@@ -30,9 +32,14 @@ const ApprovalModule = () => {
   const [stage, setStage] = useState(0);
   const [arroverSection, setApproverSection] = useState(false);
 
+  useEffect(() => {
+    console.log("show form data:", formData);
+  }, [formData]);
+
   const handleOfficeTypeChange = (e) => {
     setFormData({
       ...formData,
+      office_type: e?.value,
       operation_office_id: "",
       officeName: ""
     });
@@ -214,49 +221,12 @@ const ApprovalModule = () => {
           </div>
         </div>
 
-        {/* Offices */}
-        {/* <div className="row" style={{ paddingLeft: "1rem" }}>
-          <div className="col-md-4">
-            <div className="form-group">
-              <label htmlFor="applicable_departments">
-                Applicable Departments
-              </label>
-              <DropdownCheckbox
-                office="department"
-                options={formattedDepartmentSelection}
-                selectedOptions={selectedDepartmentOptions}
-                setSelected={setSelectedDepartment}
-                closeAll={closeAll}
-                setViewingOffice={setViewingOffice}
-                onSelectionChange={(updatedSelectedOptions) =>
-                  setSelectedDepartmentOptions(updatedSelectedOptions)
-                }
-              />
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="form-group">
-              <label htmlFor="applicable_campaigns">Applicable Campaign</label>
-              <DropdownCheckbox
-                office="campaign"
-                options={formattedCampaignSelection}
-                selectedOptions={selectedCampaignOptions}
-                setSelected={setSelectedCampaign}
-                closeAll={closeAll}
-                setViewingOffice={setViewingOffice}
-                onSelectionChange={(updatedSelectedOptions) =>
-                  setSelectedCampaignOptions(updatedSelectedOptions)
-                }
-              />
-            </div>
-          </div>
-        </div> */}
-
         <div className="col-md-4" style={{ marginTop: "2rem" }}>
           <button
             type="button"
-            className="add_btn_new"
+            className={`add_btn_new ${arroverSection && "disabled"}`}
             onClick={handleAddApprover}
+            disabled={arroverSection}
           >
             <MdOutlineAdd /> <span>Add Approver</span>
           </button>
@@ -265,10 +235,99 @@ const ApprovalModule = () => {
 
       {arroverSection && (
         <div className="column approval_builder">
-          <div className="row col-md-12">
-            radio goes here
+          <div className="form-radio-group">
+            {selectApproverOptions.map((option, index) => (
+              <div className="form-radio" key={index}>
+                <label
+                  className="form-radio-label"
+                  htmlFor={`approverOption${index}`}
+                >
+                  {option.label}
+                </label>
+                <input
+                  className="form-radio-input"
+                  type="radio"
+                  name="approverOption"
+                  id={`approverOption${index}`}
+                  value={option.value}
+                  onChange={(e) =>
+                    setFormData({
+                      ...formData,
+                      approverOption: e.target.value
+                    })
+                  }
+                />
+              </div>
+            ))}
           </div>
 
+          {/* Leadership Option */}
+          {formData?.approverOption === "leadership" ? (
+            <div className="col-md-4">
+              <div className="form-group">
+                <label htmlFor="leadership_option">Leadership Option</label>
+                <Select
+                  options={selectLeadershipOptions}
+                  isSearchable={true}
+                  value={
+                    formData?.leadership_option
+                      ? {
+                          label: formData?.leadershipName,
+                          value: formData?.leadership
+                        }
+                      : null
+                  }
+                  onChange={(e) =>
+                    setFormData({
+                      ...formData,
+                      leadershipName: e?.label,
+                      leadership: e?.value
+                    })
+                  }
+                  style={{ display: "inline-block" }}
+                  placeholder={
+                    !selectLeadershipOptions?.length
+                      ? "fetching leaderships..."
+                      : "Select Leadership"
+                  }
+                />
+              </div>
+            </div>
+          ) : null}
+
+          {/* Designation Option */}
+          {formData?.approverOption === "designation" ? (
+            <div className="col-md-4">
+              <div className="form-group">
+                <label htmlFor="designation_option">Designation Option</label>
+                <Select
+                  options={selectDesignations}
+                  isSearchable={true}
+                  value={
+                    formData?.designation_option
+                      ? {
+                          label: formData?.designationName,
+                          value: formData?.designation
+                        }
+                      : null
+                  }
+                  onChange={(e) =>
+                    setFormData({
+                      ...formData,
+                      designationName: e?.label,
+                      designation: e?.value
+                    })
+                  }
+                  style={{ display: "inline-block" }}
+                  placeholder={
+                    !selectDesignations?.length
+                      ? "fetching designation..."
+                      : "Select Designation"
+                  }
+                />
+              </div>
+            </div>
+          ) : null}
         </div>
       )}
     </>
