cd0bcd2948725f02bf9d0e997597612253bd682d
mzmarshall1278 <mzmarshall1278@gmail.com>
2024-09-17 14:56:21 +0100

    removed tabs

--- src/pages/Payroll/Allowances.js
+++ src/pages/Payroll/Allowances.js
@@ -17,42 +17,6 @@ const PayrollItems = () => {
                 </div>
             </div>
 
-            <div className="page-menu">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <ul className="nav nav-tabs nav-tabs-bottom">
-                            <li className="nav-item">
-                                <a
-                                    className="nav-link active"
-                                    data-toggle="tab"
-                                    href="#tab_allowances"
-                                >
-                                    Allowances {/* Changed to Allowances */}
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a
-                                    className="nav-link"
-                                    data-toggle="tab"
-                                    href="#tab_reversed_allowances"
-                                >
-                                    Reversed Allowances {/* Changed to Reversed Allowances */}
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a
-                                    className="nav-link"
-                                    data-toggle="tab"
-                                    href="#tab_allowance_types"
-                                >
-                                    Allowance Types {/* Changed to Allowance Types */}
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
             <div className="row tab-content">
                 <div id="tab_allowances" className="col-12 tab-pane show active">
                     <AllowancesTab />
