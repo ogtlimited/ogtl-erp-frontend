9d7c56460fe6bee5659bd07972ca3131e0af0343
najiuOutsourceGlobal <130982124+najiuOutsourceGlobal@users.noreply.github.com>
2024-09-24 10:10:19 +0100

    loan fixes

--- src/pages/Payroll/Loans.js
+++ src/pages/Payroll/Loans.js
@@ -2,8 +2,6 @@
 
 import React from "react";
 import LoansTab from "../../components/payroll-tabs/loans-tab";
-// import ReversedDeductions from "../../components/payroll-tabs/reversed-salary-deductions";
-// import DeductionType from "../../components/payroll-tabs/salary-deductiontypes";
 
 const PayrollLoans = () => {
   return (
@@ -20,54 +18,14 @@ const PayrollLoans = () => {
         </div>
       </div>
 
-      {/* <div className="page-menu">
-        <div className="row">
-          <div className="col-sm-12">
-            <ul className="nav nav-tabs nav-tabs-bottom">
-              <li className="nav-item">
-                <a
-                  className="nav-link active"
-                  data-toggle="tab"
-                  href="#tab_deductions"
-                >
-                  Loans
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  data-toggle="tab"
-                  href="#tab_reversed_deductions"
-                >
-                  Reversed Deductions
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  data-toggle="tab"
-                  href="#tab_deduction_types"
-                >
-                  Deduction Types
-                </a>
-              </li>
-            </ul>
-          </div>
+
+
+      <div className="row tab-content">
+        <div id="tab_deductions" className="col-12 tab-pane show active">
+          <LoansTab />
         </div>
-      </div> */}
 
-        <div className="row tab-content">
-          <div id="tab_deductions" className="col-12 tab-pane show active">
-            <LoansTab />
-          </div>
 
-          {/* <div id="tab_reversed_deductions" className="col-12 tab-pane">
-            <ReversedDeductions />
-          </div>
-
-        <div id="tab_deduction_types" className="col-12 tab-pane">
-          <DeductionType />
-        </div> */}
       </div>
     </>
   );
