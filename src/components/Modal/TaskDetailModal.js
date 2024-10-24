e86a75291dd31a779e9de7f07e6912f55636aeeb
najiuOutsourceGlobal <130982124+najiuOutsourceGlobal@users.noreply.github.com>
2024-10-24 18:13:41 +0100

    bugs fix on tasks

--- src/components/Modal/TaskDetailModal.js
+++ src/components/Modal/TaskDetailModal.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Modal } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import axiosInstance from '../../services/api';
 import { useAppContext } from '../../Context/AppContext';
+import { isToday } from 'date-fns';
 
 
-function TaskDetailModal({ showModal, setShowModal, selectedTask, dailyTasks, fetchData, ogId }) {
+function TaskDetailModal({ showModal, setShowModal, selectedTask, dailyTasks, fetchData, ogId, loading }) {
     const { user } = useAppContext();
+    const [confirmLoading, setConfirmLoading] = useState(false);
 
     const employeeOgId = user?.employee_info?.ogid;
 
@@ -26,12 +28,16 @@ function TaskDetailModal({ showModal, setShowModal, selectedTask, dailyTasks, fe
     }
     // Function to update the task on the server
     const updateTask = async (taskId, payload) => {
+        setConfirmLoading(true)
         try {
             await axiosInstance.patch(`/api/v1/employee_tasks/${taskId}`, { payload });
             console.log("Task updated successfully:", payload);
         } catch (error) {
             console.error("Error updating task:", error);
         }
+
+
+        setConfirmLoading(false)
     };
 
     // Handle checkbox toggle without immediately confirming (just reflects completion status)
@@ -42,6 +48,8 @@ function TaskDetailModal({ showModal, setShowModal, selectedTask, dailyTasks, fe
 
         // Call the API to update the "completed" status
         updateTask(taskId, { complete: completed });
+
+
     };
 
     // Handle notes change and send update to the server when note is changed
@@ -83,66 +91,82 @@ function TaskDetailModal({ showModal, setShowModal, selectedTask, dailyTasks, fe
             <Modal.Header>
                 <Modal.Title>Task Details</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-                <div>
-                    <h5>Daily Tasks</h5>
-                    {tasks.length === 0 ? (
-                        <p>No tasks available for this date.</p>
-                    ) : (
-                        tasks.map((task) => (
-                            <div key={task.id} className="task-item mb-3 p-3 border rounded">
-                                <div className="d-flex align-items-center justify-content-between">
-                                    <div>
-                                        {
-                                            ogId === employeeOgId &&
 
-                                            <input
-                                                type="checkbox"
-                                                checked={task.confirmed || false} // Use task.completed for checkbox
-                                                onChange={(e) => handleCheckboxChange(task.id, e.target.checked)}
-                                                className="me-2 mr-4"
-                                                disabled
+            {
+                loading ? <Modal.Body>
+
+                    <span
+                        className="spinner-border spinner-border-sm"
+                        role="status"
+                        aria-hidden="true"
+                    ></span>
+                </Modal.Body> :
+
+                    <Modal.Body>
+                        <div>
+                            <h5>Daily Tasks</h5>
+                            {tasks.length === 0 ? (
+                                <p>No tasks available for this date.</p>
+                            ) : (
+                                tasks.map((task) => (
+                                    <div key={task.id} className="task-item mb-3 p-3 border rounded">
+                                        <div className="d-flex align-items-center justify-content-between">
+                                            <div>
+                                                {
+                                                    ogId === employeeOgId &&
+
+                                                    <input
+                                                        type="checkbox"
+                                                        checked={task.confirmed || false} // Use task.completed for checkbox
+                                                        onChange={(e) => handleCheckboxChange(task.id, e.target.checked)}
+                                                        className="me-2 mr-4"
+                                                        disabled
+                                                    />
+                                                }
+
+
+                                                <span>{task.taskName}</span>
+                                            </div>
+                                            <div className="task-date">
+                                                <small>{task.taskDate}</small>
+                                            </div>
+                                            <div className="action-buttons">
+                                                {
+                                                    ogId === employeeOgId && isToday(task.taskDate) && <Button
+                                                        className={`btn btn-sm btn-${task.confirmed ? "danger" : "primary"}`}
+                                                        onClick={() => handleConfirmOrUndo(task.id)}
+                                                        loading={confirmLoading}
+                                                    >
+                                                        {task.confirmed ? "Undo" : "Confirm"}
+                                                    </Button>
+                                                }
+
+                                            </div>
+                                        </div>
+                                        <div className="mt-2">
+                                            <textarea
+                                                className="form-control"
+                                                placeholder=""
+                                                value={task.notes}
+                                                onChange={(e) => handleNotesChange(task.id, e.target.value)}
+                                                rows={3}
+                                                disabled={!ogId || ogId !== employeeOgId || task.confirmed}
                                             />
-                                        }
-
-
-                                        <span>{task.taskName}</span>
+                                        </div>
                                     </div>
-                                    <div className="task-date">
-                                        <small>{task.taskDate}</small>
-                                    </div>
-                                    <div className="action-buttons">
-                                        {
-                                            ogId === employeeOgId && <button
-                                                className={`btn btn-sm btn-${task.confirmed ? "danger" : "primary"}`}
-                                                onClick={() => handleConfirmOrUndo(task.id)}
-                                            >
-                                                {task.confirmed ? "Undo" : "Confirm"}
-                                            </button>
-                                        }
+                                ))
+                            )}
+                        </div>
+                    </Modal.Body>
 
-                                    </div>
-                                </div>
-                                <div className="mt-2">
-                                    <textarea
-                                        className="form-control"
-                                        placeholder=""
-                                        value={task.notes}
-                                        onChange={(e) => handleNotesChange(task.id, e.target.value)}
-                                        rows={3}
-                                        disabled={!ogId || ogId !== employeeOgId || task.confirmed}
-                                    />
-                                </div>
-                            </div>
-                        ))
-                    )}
-                </div>
-            </Modal.Body>
+            }
             <Modal.Footer>
                 <button className="btn btn-secondary" onClick={() => closeModal()}>
                     Close
                 </button>
             </Modal.Footer>
+
+
         </Modal>
     );
 }
